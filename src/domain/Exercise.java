ad6cd3e12cc0356bdab8ab71dadf79fd7861e936
Raakkaani <lirodaki345@gmail.com>
2026-05-25 21:02:42 +0300

    зміна файлу Exercise

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,13 @@
 package domain;
 
+import java.util.Arrays;
+
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static int Calculate(int[] array){
+        if(array.length<2){
+            return 0;
+        }
+        Arrays.sort(array);
+        return array[array.length-1];
     }
 }
